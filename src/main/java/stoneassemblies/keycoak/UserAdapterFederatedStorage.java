83b4a5464315f86e02c8a58c1e2090405bd618d2
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-05-26 12:21:32 -0400

    Remove attribute for roles before set, in order to override its value.

--- src/main/java/stoneassemblies/keycoak/UserAdapterFederatedStorage.java
+++ src/main/java/stoneassemblies/keycoak/UserAdapterFederatedStorage.java
@@ -7,13 +7,14 @@ import org.keycloak.models.RealmModel;
 import org.keycloak.storage.StorageId;
 import org.keycloak.storage.adapter.AbstractUserAdapterFederatedStorage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class UserAdapterFederatedStorage extends AbstractUserAdapterFederatedStorage {
 
     private final User user;
+
+    static final String FEDERATED_ROLES = "FederatedRoles";
 
     public UserAdapterFederatedStorage(KeycloakSession session, RealmModel realm, ComponentModel model, User user) {
         super(session, realm, model);
@@ -24,8 +25,14 @@ public class UserAdapterFederatedStorage extends AbstractUserAdapterFederatedSto
         setEnabled(user.isEnabled());
         setCreatedTimestamp(user.getCreated());
         List<String> roles = user.getRoles();
-        if(roles != null){
-            this.setAttribute("FederatedRoles", roles);
+        if (roles != null) {
+            try {
+                this.removeAttribute(FEDERATED_ROLES);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            this.setAttribute(FEDERATED_ROLES, roles);
         }
     }
 
