92f60dda32ed3b66465b4d4057634b884c400dce
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-11-07 12:49:41 -0500

    Adds ignore attribute to tests

--- src/test/java/RabbitMqClientTests.java
+++ src/test/java/RabbitMqClientTests.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import stoneassemblies.keycoak.AesEncryptionService;
 import stoneassemblies.keycoak.RabbitMqUserRepository;
@@ -20,7 +21,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class RabbitMqClientTests {
+
     @Test
+    @Ignore
     public void getUsersCount() {
         RabbitMqUserRepository rabbitMqUserRepository = new RabbitMqUserRepository("localhost", 6002, "public", "queuedemo", "queuedemo", 10, new AesEncryptionService("sOme*ShaREd*SecreT"));
         int usersCount = rabbitMqUserRepository.getUsersCount();
@@ -28,6 +31,7 @@ public class RabbitMqClientTests {
     }
 
     @Test
+    @Ignore
     public void findUserById() {
         RabbitMqUserRepository rabbitMqUserRepository = new RabbitMqUserRepository("localhost", 6002, "public", "queuedemo", "queuedemo", 10, new AesEncryptionService("sOme*ShaREd*SecreT"));
         User user = rabbitMqUserRepository.findUserByUsernameOrEmail("jane.doe0");
@@ -36,6 +40,7 @@ public class RabbitMqClientTests {
     }
 
     @Test
+    @Ignore
     public void findUserByUsernameOrEmail() {
         RabbitMqUserRepository rabbitMqUserRepository = new RabbitMqUserRepository("localhost", 6002, "public", "queuedemo", "queuedemo", 10, new AesEncryptionService("sOme*ShaREd*SecreT"));
         User user = rabbitMqUserRepository.findUserByUsernameOrEmail("jane.doe0");
@@ -43,6 +48,7 @@ public class RabbitMqClientTests {
     }
 
     @Test
+    @Ignore
     public void getUsers() {
         RabbitMqUserRepository rabbitMqUserRepository = new RabbitMqUserRepository("localhost", 6002, "public", "queuedemo", "queuedemo", 10, new AesEncryptionService("sOme*ShaREd*SecreT"));
         List<User> users = rabbitMqUserRepository.getUsers(39, 20);
@@ -53,6 +59,7 @@ public class RabbitMqClientTests {
     }
 
     @Test
+    @Ignore
     public void sequentialValidateCredentials() {
         RabbitMqUserRepository rabbitMqUserRepository = new RabbitMqUserRepository("localhost", 6002, "public", "queuedemo", "queuedemo", 10, new AesEncryptionService("sOme*ShaREd*SecreT"));
         for (int i = 0; i < 5; i++) {
@@ -61,12 +68,13 @@ public class RabbitMqClientTests {
     }
 
     @Test
+    @Ignore
     public void parallelValidateCredentials() {
         long startTime = System.nanoTime();
         RabbitMqUserRepository rabbitMqUserRepository = new RabbitMqUserRepository("localhost", 6002, "public", "queuedemo", "queuedemo", 60, new AesEncryptionService("sOme*ShaREd*SecreT"));
         ExecutorService executorService = Executors.newCachedThreadPool();
         AtomicInteger count = new AtomicInteger();
-        int expected = 30;
+        int expected = 100;
         for (int i = 0; i < expected; i++) {
             int finalI = i;
             executorService.execute(() -> {
@@ -91,95 +99,10 @@ public class RabbitMqClientTests {
     }
 
     @Test
+    @Ignore
     public void updateCredentials() {
         RabbitMqUserRepository rabbitMqUserRepository = new RabbitMqUserRepository("localhost", 6002, "public", "queuedemo", "queuedemo", 10, new AesEncryptionService("sOme*ShaREd*SecreT"));
-//        boolean succeeded = rabbitMqUserRepository.updateCredentials("juan", "Password123!");
-    }
-
-    @Test
-    public void sendAndReceive() throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setUsername("syncbee");
-        factory.setPassword("syncbee");
-
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-
-            String queueName = "login-queue-3";
-//            String callbackQueueName = "login-queue-3-callback";
-//            channel.queueDeclare(callbackQueueName, true, false, false, null);
-
-            channel.queueDeclare(queueName, true, false, false, null);
-            String exchange0 = "ConsoleApp11:ValueMessage";
-            String exchange1 = "ConsoleApp11:WelcomeBack";
-
-            channel.exchangeDeclare(exchange0, BuiltinExchangeType.FANOUT, true);
-            channel.exchangeDeclare(exchange1, BuiltinExchangeType.FANOUT, true);
-
-            channel.queueBind(queueName, exchange0, "");
-            channel.queueBind(queueName, exchange1, "");
-
-            AtomicBoolean received = new AtomicBoolean(false);
-
-            UUID correlationId = UUID.randomUUID();
-            channel.basicConsume(queueName, false, (consumerTag, message) -> {
-                try {
-                    String s = new String(message.getBody());
-                    JSONObject jsonObject = new JSONObject(s);
-                    UUID receivedCorrelationId = UUID.fromString(jsonObject.getJSONObject("message").getString("correlationId"));
-                    if (receivedCorrelationId.equals(correlationId)) {
-                        System.out.println(consumerTag);
-                        System.out.println(s);
-                        long deliveryTag = message.getEnvelope().getDeliveryTag();
-                        channel.basicAck(deliveryTag, false);
-                        received.set(true);
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }, consumerTag -> {
-                System.out.println(consumerTag);
-            });
-
-
-            String message = "{\n" +
-                    "    \"message\": {\n" +
-                    "        \"value\": \"Some Value\",\n" +
-                    "        \"correlationId\": \"" + correlationId + "\",\n" +
-                    "        \"customerId\": 27\n" +
-                    "    },\n" +
-                    "    \"messageType\": [\n" +
-                    "        \"urn:message:ConsoleApp11:ValueMessage\"\n" +
-                    "    ]\n" +
-                    "}";
-
-//            String correlationId = UUID.randomUUID().toString();
-//            AMQP.BasicProperties build = new AMQP.BasicProperties.Builder()
-//                    .replyTo(queueName)
-//                    .correlationId(correlationId)
-//                    .build();
-
-            channel.basicPublish(exchange0, queueName, null, message.getBytes());
-
-//            RpcClientParams params = new RpcClientParams();
-//            params.channel(channel);
-//            params.exchange(exchange0);
-//            params.routingKey("");
-//            params.replyTo(queueName);
-//            params.correlationIdSupplier(() -> "AAAAA");
-//
-//            RpcClient rpcClient = new RpcClient(params);
-//            RpcClient.Response response = rpcClient.doCall(null, message.getBytes());
-
-            while (!received.get()) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        boolean succeeded = rabbitMqUserRepository.updateCredentials("juan", "Password123!");
     }
 }
 
